d0acd2a3fc428b4d5ef27778b747ebf6a73a34af
verdantpath <m_dittmann@yahoo.com>
2022-04-13 21:57:47 -0700

    Add computed property

--- vue-3/app.js
+++ vue-3/app.js
@@ -3,26 +3,26 @@ const app = Vue.createApp({
   data() {
     return {
       showBooks: true,
-      title: 'The Way of Kings',
-      author: 'Brandon Sanderson',
-      age: 45,
-      x: 0,
-      y: 0,
+      books: [
+        { title: 'name of the wind', author: 'patrick rothfuss', img: 'assets/1.jpg', isFav: true },
+        { title: 'the way of kings', author: 'brandon sanderson', img: 'assets/2.jpg', isFav: false },
+        { title: 'the final empire', author: 'brandon sanderson', img: 'assets/3.jpg', isFav: true },
+      ],
     }
   },
   methods: {
     toggleShowBooks() {
       this.showBooks =  !this.showBooks
     },
-    handleEvent(e, data) {
-      console.log(e.type, e)
-      if (data) {
-        console.log(data)
-      }
-    },
-    handleMousemove(e) {
-      this.x = e.offsetX
-      this.y = e.offsetY
+    toggleFav(book) {
+      book.isFav = !book.isFav
+    }
+  },
+  computed: {
+    filteredBooks() {
+      return this.books.filter((book) => {
+        return book.isFav
+      })
     }
   }
 })
