4788c79aba00884ac576b03f415cc0c2661623ec
verdantpath <m_dittmann@yahoo.com>
2022-04-25 19:19:13 -0700

    Add props and slots

--- vue-3/modal-project/src/App.vue
+++ vue-3/modal-project/src/App.vue
@@ -1,15 +1,39 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <h1>{{ title }}</h1>
+  <div v-if="showModal">  
+    <Modal theme="sale" @close="toggleModal">
+      <h1>Ninja Giveaway</h1>
+      <p>Grab your ninja swag for half price!</p>
+      <template v-slot:links>
+        <a href="#">Sign-up now</a>
+        <a href="#">more info</a>
+      </template>
+    </Modal>
+  </div>
+  <p>welcome</p>
+  <button @click="toggleModal">open modal</button>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Modal from './components/Modal.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Modal
+  },
+  data() {
+    return {
+      title: 'My First Vue App',
+      header: 'Sign up for the giveaway!!!',
+      text: 'Grab your ninja swag for half-price',
+      showModal: false
+    }
+  },
+  methods: {
+    toggleModal() {
+      this.showModal = !this.showModal;
+    }
   }
 }
 </script>
@@ -22,5 +46,10 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+h1 {
+  border-bottom: 1px solid #ddd;
+  display: inline-block;
+  padding-bottom: 10px;
 }
 </style>
