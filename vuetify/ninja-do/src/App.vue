5f5a91cdf03a795066802d37f6a200a4b577db90
verdantpath <m_dittmann@yahoo.com>
2022-05-10 20:14:36 -0700

    Initial commit

---
+++ vuetify/ninja-do/src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <v-app>
+    <v-toolbar app dark class="indigo">
+      <v-toolbar-title>
+        Vuetify
+      </v-toolbar-title>
+    </v-toolbar>
+
+    <v-content>
+      <router-view></router-view>
+    </v-content>
+    
+  </v-app>
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+  data () {
+    return {
+      
+    }
+  }
+}
+</script>
