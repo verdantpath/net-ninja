24660074f96462be7855304f567df21e2adc68c8
verdantpath <m_dittmann@yahoo.com>
2024-03-25 17:33:00 -0700

    Add App

---
+++ vue-3/dojo-blog/src/App.vue
@@ -0,0 +1,22 @@
+<script setup>
+
+</script>
+
+<template>
+  <router-view />
+</template>
+
+<style>
+@import '@/assets/base.css';
+
+#app {
+  max-width: 1280px;
+  margin: 0 auto;
+  padding: 2rem;
+
+  font-weight: normal;
+  text-align: center;
+}
+
+
+</style>
