4c09b2dddedd9bf92d882e3a01b0130470861013
verdantpath <m_dittmann@yahoo.com>
2024-03-27 20:03:53 -0700

    Add Create view

---
+++ vue-3/dojo-blog/src/views/Create.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="create">
+    <form action="">
+      <label>Title</label>
+      <input type="text" required>
+      <label>Content</label>
+      <textarea></textarea>
+    </form>
+  </div>
+</template>
+<script>
+  export default {
+    
+  }
+</script>
+<style>
+
+</style>
