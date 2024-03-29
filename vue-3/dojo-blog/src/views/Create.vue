f1d7803c364bbcaf996bc33c900ecc1963dfa181
verdantpath <m_dittmann@yahoo.com>
2024-03-29 16:50:21 -0700

    Update Create view

--- vue-3/dojo-blog/src/views/Create.vue
+++ vue-3/dojo-blog/src/views/Create.vue
@@ -2,15 +2,27 @@
   <div class="create">
     <form action="">
       <label>Title</label>
-      <input type="text" required>
+      <input v-model="title" type="text" required>
       <label>Content</label>
-      <textarea></textarea>
+      <textarea v-model="body" required></textarea>
+      <label>Tags (hit enter to add a tag)</label>
+      <input v-model="tag" type="text">
+      <button>Add Post</button>
     </form>
   </div>
 </template>
 <script>
+import { ref } from 'vue';
+
   export default {
-    
+    setup() {
+      const title = ref('')
+      const body = ref('')
+      const tag = ref('')
+      const tags = ref([])
+
+      return { title, body, tag }
+    }
   }
 </script>
 <style>
