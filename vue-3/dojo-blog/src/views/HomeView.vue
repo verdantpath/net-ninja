790254795cfec2bbead210a2b856d8e09e29880f
verdantpath <m_dittmann@yahoo.com>
2024-03-30 12:18:50 -0700

    Add HomeView view

---
+++ vue-3/dojo-blog/src/views/HomeView.vue
@@ -0,0 +1,34 @@
+<template>
+  <main>
+    <h1>Home</h1>
+    <div v-if="error">{{ error }}</div>
+    <div v-if="posts.length">
+      <PostList :posts="posts" />
+    </div>
+    <div v-else>
+      <Spinner />
+    </div>
+    
+  </main>
+</template>
+
+
+<script>
+import PostList from '../components/PostList.vue'
+import getPosts from '../composables/getPosts'
+import Spinner from '../components/Spinner.vue'
+
+export default {
+  name: 'Home',
+  components: { PostList, Spinner },
+  setup() {
+    const { posts, error, load } = getPosts()
+
+    load()
+
+    return { posts, error }
+
+  },
+}
+</script>
+
