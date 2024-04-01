a14c7d21b3d6e3ce8432e4d63adb32628c4fa4ff
verdantpath <m_dittmann@yahoo.com>
2024-03-31 20:13:53 -0700

    Update create view

--- vue-3/dojo-blog/src/views/Create.vue
+++ vue-3/dojo-blog/src/views/Create.vue
@@ -1,12 +1,17 @@
 <template>
   <div class="create">
-    <form action="">
+    <form @submit.prevent="handleSubmit">
       <label>Title</label>
       <input v-model="title" type="text" required>
       <label>Content</label>
       <textarea v-model="body" required></textarea>
       <label>Tags (hit enter to add a tag)</label>
-      <input v-model="tag" type="text">
+      <input 
+        v-model="tag" 
+        @keydown.enter.prevent="handleKeydown" 
+        type="text"
+      >
+      <div v-for="tag in tags" :key="tag" class="pill">#{{ tag }}</div>
       <button>Add Post</button>
     </form>
   </div>
@@ -21,10 +26,84 @@ import { ref } from 'vue';
       const tag = ref('')
       const tags = ref([])
 
-      return { title, body, tag }
+      const handleKeydown = () => {
+        if (!tags.value.includes(tag.value)) {
+          tag.value = tag.value.replace(/\s/, '') // removes all whitespace
+          tags.value.push(tag.value)
+        }
+        tag.value = ''
+      }
+
+      const handleSubmit = async () => {
+        const post = {
+          title: title.value,
+          body: body.value,
+          tags: tags.value
+        }
+        await fetch('http://localhost:3001/posts', {
+          method: 'POST',
+          headers: { 'Content-Type': 'application/json' },
+          body: JSON.stringify(post) 
+        })
+      }
+
+      return { title, body, tag, handleKeydown, tags, handleSubmit }
     }
   }
 </script>
 <style>
-
+  form {
+    max-width: 480px;
+    margin: 0 auto;
+    text-align: left;
+  }
+  input, textarea {
+    display: block;
+    margin: 10px 0;
+    width: 100%;
+    box-sizing: border-box;
+    padding: 10px;
+    border: 1px solid #eee;
+  }
+  textarea {
+    height: 160px;
+  }
+  label {
+    display: inline-block;
+    margin-top: 30px;
+    position: relative;
+    font-size: 20px;
+    color: white;
+    margin-bottom: 10px;
+  }
+  label::before {
+    content: "";
+    display: block;
+    width: calc(100% + 70px);
+    height: 100%;
+    background: #ff8800;
+    position: absolute;
+    z-index: -1;
+    /*padding-right: 70px;*/
+    left: -30px;
+    transform: rotateZ(-1.5deg);
+  }
+  button {
+    display: block;
+    margin-top: 30px;
+    background: #ff8800;
+    color: white;
+    border: none;
+    padding: 8px 16px;
+    font-size: 18px
+  }
+  .pill {
+    display: inline-block;
+    margin: 10px 10px 0 0;
+    color: #444;
+    background: #ddd;
+    padding: 8px;
+    border-radius: 20px;
+    font-size: 14px;
+  }
 </style>
