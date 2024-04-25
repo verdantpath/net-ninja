47847492812eab916b4382451d5ba4b3072c04cd
verdantpath <m_dittmann@yahoo.com>
2024-04-25 15:53:38 -0700

    Update router

--- vue-3/dojo-blog/src/router/index.js
+++ vue-3/dojo-blog/src/router/index.js
@@ -2,6 +2,7 @@ import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import Details from '../views/Details.vue'
 import Create from '../views/Create.vue'
+import Tag from '../views/Tag.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -21,6 +22,11 @@ const router = createRouter({
       path: '/create',
       name: 'Create',
       component: Create
+    },
+    {
+      path: '/tags/:tag',
+      name: 'Tag',
+      component: Tag
     }
   ]
 })
