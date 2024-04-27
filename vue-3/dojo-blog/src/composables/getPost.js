93fed0cf93d90463bb838f9609e500b682782cbb
verdantpath <m_dittmann@yahoo.com>
2024-04-27 00:17:17 -0700

    Update getPost

--- vue-3/dojo-blog/src/composables/getPost.js
+++ vue-3/dojo-blog/src/composables/getPost.js
@@ -1,4 +1,5 @@
 import { ref } from 'vue'
+import { projectFirestore } from '../firebase/config'
 
 const getPost = (id) => {
   const post = ref(null)
@@ -6,12 +7,14 @@ const getPost = (id) => {
   
   const load = async () => {
     try {
-      
-      let data = await fetch('http://localhost:3001/posts/' + id)
-      if (!data.ok) {
-        throw Error('that post does not exist')
+      let res = await projectFirestore.collection('posts').doc(id).get()
+
+      if(!res.exists) {
+        throw Error('That post does not exist')
       }
-      post.value = await data.json()
+
+      post.value = { ...res.data(), id: res.id }
+      console.log(post.value)
     }
     catch (err) {
       error.value = err.message
