b54e8ef5ef2453072c17f0feac20976d9a6c7bbf
verdantpath <m_dittmann@yahoo.com>
2024-04-28 20:38:20 -0700

    Update getPosts composabale function

--- vue-3/dojo-blog/src/composables/getPosts.js
+++ vue-3/dojo-blog/src/composables/getPosts.js
@@ -1,4 +1,5 @@
 import { ref } from 'vue'
+import { projectFirestore } from '../firebase/config'
 
 const getPosts = () => {
   const posts = ref([])
@@ -6,12 +7,12 @@ const getPosts = () => {
   
   const load = async () => {
     try {
+      const res = await projectFirestore.collection('posts').get()
       
-      let data = await fetch('http://localhost:3001/posts')
-      if (!data.ok) {
-        throw Error('no data available')
-      }
-      posts.value = await data.json()
+      posts.value = res.docs.map(doc => {
+        // console.log(doc.data())
+        return { ...doc.data(), id: doc.id }
+      })
     }
     catch (err) {
       error.value = err.message
