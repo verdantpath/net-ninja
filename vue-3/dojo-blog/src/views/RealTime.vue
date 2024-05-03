8da10fcf870cfba74955f9880a8eca0016fd93a3
verdantpath <m_dittmann@yahoo.com>
2024-05-02 17:52:52 -0700

    Add RealTime view

---
+++ vue-3/dojo-blog/src/views/RealTime.vue
@@ -0,0 +1,21 @@
+<template>
+  <h1>Real-time data</h1>
+</template>
+<script>
+import { ref } from 'vue';
+
+  export default {
+    setup() {
+      const posts = ref([])
+
+      projectFirestore.collection('posts')
+        .orderBy('createdAt', 'desc')
+        .onSnapshot((snap) => {
+          console.log(snap)
+        })
+    }
+  }
+</script>
+<style>
+
+</style>
