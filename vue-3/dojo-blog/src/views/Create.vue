aabee2eafc6353f7eab1c0c3afe61267b073016a
verdantpath <m_dittmann@yahoo.com>
2024-05-07 10:00:51 -0700

    Update Create view

--- vue-3/dojo-blog/src/views/Create.vue
+++ vue-3/dojo-blog/src/views/Create.vue
@@ -19,6 +19,7 @@
 <script>
 import { ref } from 'vue';
 import { useRouter } from 'vue-router';
+import { projectFirestore, timestamp } from '../firebase/config';
 
   export default {
     setup() {
@@ -42,13 +43,11 @@ import { useRouter } from 'vue-router';
         const post = {
           title: title.value,
           body: body.value,
-          tags: tags.value
+          tags: tags.value,
+          createdAt: timestamp()
         }
-        await fetch('http://localhost:3001/posts', {
-          method: 'POST',
-          headers: { 'Content-Type': 'application/json' },
-          body: JSON.stringify(post) 
-        })
+        const res = await projectFirestore.collection('posts').add(post)
+        // console.log(res)
 
         router.push({ name: 'home' })
       }
