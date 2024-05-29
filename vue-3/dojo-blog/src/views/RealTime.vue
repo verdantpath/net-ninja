fd8a3170c98f12c075ae1e916e9e52a51644c98f
verdantpath <m_dittmann@yahoo.com>
2024-05-28 21:58:56 -0700

    Update RealTime view

--- vue-3/dojo-blog/src/views/RealTime.vue
+++ vue-3/dojo-blog/src/views/RealTime.vue
@@ -1,8 +1,11 @@
 <template>
   <h1>Real-time data</h1>
+  <div v-for="post in posts" :key="post.id"><div>{{ post.title }}</div></div>
+  
 </template>
 <script>
 import { ref } from 'vue';
+import { projectFirestore } from '../firebase/config'
 
   export default {
     setup() {
@@ -11,8 +14,13 @@ import { ref } from 'vue';
       projectFirestore.collection('posts')
         .orderBy('createdAt', 'desc')
         .onSnapshot((snap) => {
-          console.log(snap)
+          let docs = snap.docs.map(doc => {
+            return { ...doc.data(), id: doc.id}
+          })
+          console.log('documents: ', docs)
+          posts.value = docs
         })
+        return { posts }
     }
   }
 </script>
