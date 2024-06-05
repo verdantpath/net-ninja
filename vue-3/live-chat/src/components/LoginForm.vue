bb715cfd703afad0d49ae3476a0800930c3ef1eb
verdantpath <m_dittmann@yahoo.com>
2024-06-04 20:34:14 -0700

    Add login component

---
+++ vue-3/live-chat/src/components/LoginForm.vue
@@ -0,0 +1,34 @@
+<template>
+  <form @submit.prevent="handleSubmit">
+    <input type="email" required placeholder="email" v-model="email">
+    <input type="password" required placeholder="password" v-model="password">
+    <div class="error">{{ error }}</div>
+    <button>Log in</button>
+  </form>
+</template>
+<script>
+import { ref } from 'vue';
+import useLogin from '../composables/useLogin'
+
+  export default {
+    setup() {
+      // refs
+      const email = ref('')
+      const password = ref('')
+
+      const { error, login } = useLogin()
+
+      const handleSubmit = async () => {
+        await login(email.value, password.value)
+        if (!error.value) {
+          console.log('user logged in')
+        }
+      }
+
+      return { email, password, handleSubmit, error }
+    }
+  }
+</script>
+<style>
+
+</style>
