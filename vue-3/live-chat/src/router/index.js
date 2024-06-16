127599b64cdbcbea5a0e07eb66bb83b8682ede84
verdantpath <m_dittmann@yahoo.com>
2024-06-16 10:42:39 -0700

    Update router

---
+++ vue-3/live-chat/src/router/index.js
@@ -0,0 +1,37 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import Welcome from '../views/Welcome.vue'
+import Chatroom from '../views/Chatroom.vue'
+import { projectAuth } from '../firebase/config'
+
+// auth guard
+const requireAuth = (t9, from, next) => {
+  let user = projectAuth.currentUser
+  console.log('current user in auth guard: ', user.displayName, user)
+  if (!user) {
+    next({ name: 'Welcome' })
+  } else {
+    next()
+  }
+}
+
+const routes = [
+  {
+    path: '/',
+    name: 'Welcome',
+    component: Welcome
+  },
+  {
+    path: '/chatroom', 
+    name: 'Chatroom', 
+    component: Chatroom,
+    beforeEnter: requireAuth
+  }
+]
+
+const router = createRouter({
+  // history: createWebHistory(),
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes
+})
+
+export default router
