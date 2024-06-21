6d0a2271894aa50555f484c763231fcf5d876dda
verdantpath <m_dittmann@yahoo.com>
2024-06-21 10:15:19 -0700

    Update Chatroom view

--- vue-3/live-chat/src/views/Chatroom.vue
+++ vue-3/live-chat/src/views/Chatroom.vue
@@ -1,12 +1,26 @@
 <template>
   <div class="container">
     <Navbar />
+    <NewChatForm />
   </div>
 </template>
 <script>
+import { watch } from 'vue';
+import { useRouter } from 'vue-router'
 import Navbar from '../components/Navbar.vue'
+import getUser from '../composables/getUser'
+import NewChatForm from '../components/NewChatForm.vue'
   export default {
-    components: { Navbar }
+    components: { Navbar, NewChatForm },
+    setup() {
+      const { user } = getUser()
+      const router = useRouter()
+      watch(user, () => {
+        if (!user.value) {
+          router.push({ name: 'Welcome' })
+        }
+      })
+    }
   }
 </script>
 <style>
