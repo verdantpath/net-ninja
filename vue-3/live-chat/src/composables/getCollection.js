9db615a43a4be6ff20d297ce0b4425f70211f715
verdantpath <m_dittmann@yahoo.com>
2024-07-04 21:31:51 -0700

    Update getCollection function

--- vue-3/live-chat/src/composables/getCollection.js
+++ vue-3/live-chat/src/composables/getCollection.js
@@ -1,4 +1,4 @@
-import { ref } from 'vue'
+import { ref, watchEffect } from 'vue'
 import { projectFirestore } from '../firebase/config'
 
 const getCollection = (collection) => {
@@ -8,18 +8,26 @@ const getCollection = (collection) => {
   let collectionRef = projectFirestore.collection(collection)
     .orderBy('createdAt')
 
-
-    collectionRef.onSnapshot((snap) => {
+    const unsub = collectionRef.onSnapshot((snap) => {
+      console.log('snapshot taken')
       let results = []
+      console.log('snap.docs:', snap.docs)
       snap.docs.forEach((doc) => {
+        //console.log('doc:', doc.data())
         doc.data().createdAt && results.push({ ...doc.data(), id: doc.id })
       })
       documents.value = results
+      console.log('results:', results);
       error.value = null
     }, (err) => {
       console.log(err.message)
       documents.value = null
       error.value = 'could not fetch data'
+    })
+
+    watchEffect((onInvalidate) => {
+      // unsub from prev collection when watcher is stopped (component unmounted)
+      onInvalidate(() => unsub())
     })
 
     return { documents, error }
