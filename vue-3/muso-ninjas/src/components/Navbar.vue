a68764ece3e78ae3f080bc0eb031635acb1bcaee
verdantpath <m_dittmann@yahoo.com>
2024-08-03 20:02:59 -0700

    Add Navbar

---
+++ vue-3/muso-ninjas/src/components/Navbar.vue
@@ -0,0 +1,67 @@
+<template>
+  <div class="navbar">
+    <nav>
+      <img src="@/assets/ninja.png" alt="ninjas" />
+      <h1><router-link :to="{ name: 'home' }">Muso Ninjas</router-link></h1>
+      <div class="links">
+        <div v-if="user">
+          <button @click="handleSubmit">Logout</button>
+        </div>
+        <div v-else>
+          <router-link class="btn" :to="{ name: 'Signup' }">Signup</router-link>
+          <router-link class="btn" :to="{ name: 'Login' }">Login</router-link>
+        </div>
+      </div>
+    </nav>
+  </div>
+</template>
+<script>
+import { ref } from 'vue';
+import useLogout from '../composables/useLogout'
+import getUser from '../composables/getUser'
+import { useRouter } from 'vue-router'
+
+  export default {
+    setup() {
+      const { logout, error, isPending } = useLogout()
+      const { user } = getUser()
+      const router = useRouter()
+
+      const handleSubmit = async () => {
+        await logout()
+        console.log('user logged out: ', user)
+        router.push({name: 'Login'})
+      }
+
+      return { handleSubmit, user }
+    }
+  }
+</script>
+<style scoped>
+.navbar {
+  padding: 16px;
+  margin-bottom: 60px;
+  background: white;
+}
+nav {
+  display: flex;
+  align-items: center;
+  max-width: 1200px;
+  margin: 0 auto;
+}
+nav h1 {
+  margin-left: 20px;
+
+}
+nav .links {
+  margin-left: auto;
+}
+nav .links a, button {
+  margin-left: 16px;
+  font-size: 14px;
+}
+nav img {
+  max-height: 60px;
+}
+
+</style>
