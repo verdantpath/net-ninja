27c357db141dfc654142381523c18d365a811ce1
verdantpath <m_dittmann@yahoo.com>
2024-08-06 20:00:29 -0700

    add router

---
+++ vue-3/muso-ninjas/src/router/index.js
@@ -0,0 +1,33 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import HomeView from '../views/HomeView.vue'
+import Login from '../views/auth/Login.vue'
+import Signup from '../views/auth/Signup.vue'
+import CreatePlaylist from '../views/playlists/CreatePlaylist.vue'
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: HomeView
+    },
+    {
+      path: '/login',
+      name: 'Login',
+      component: Login
+    }, 
+    {
+      path: '/signup',
+      name: 'Signup',
+      component: Signup
+    },
+    {
+      path: '/playlists/create',
+      name: 'CreatePlaylist',
+      component: CreatePlaylist
+    }
+  ]
+})
+
+export default router
