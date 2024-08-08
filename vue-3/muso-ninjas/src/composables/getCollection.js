6e1e1d5729232b81b2290441974ba817639ee159
verdantpath <m_dittmann@yahoo.com>
2024-08-07 19:57:21 -0700

    Add getCollection function

---
+++ vue-3/muso-ninjas/src/composables/getCollection.js
@@ -0,0 +1,36 @@
+import { ref, watchEffect } from 'vue'
+import { projectFirestore } from '../firebase/config'
+
+const getCollection = (collection) => {
+  const documents = ref(null)
+  const error = ref(null)
+
+  let collectionRef = projectFirestore.collection(collection)
+    .orderBy('createdAt')
+
+    const unsub = collectionRef.onSnapshot((snap) => {
+      console.log('snapshot taken')
+      let results = []
+      console.log('snap.docs:', snap.docs)
+      snap.docs.forEach((doc) => {
+        //console.log('doc:', doc.data())
+        doc.data().createdAt && results.push({ ...doc.data(), id: doc.id })
+      })
+      documents.value = results
+      console.log('results:', results);
+      error.value = null
+    }, (err) => {
+      console.log(err.message)
+      documents.value = null
+      error.value = 'could not fetch data'
+    })
+
+    watchEffect((onInvalidate) => {
+      // unsub from prev collection when watcher is stopped (component unmounted)
+      onInvalidate(() => unsub())
+    })
+
+    return { documents, error }
+}
+
+export default getCollection
