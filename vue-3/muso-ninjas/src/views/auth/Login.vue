e852e9293e8080d0386b07f3f78e2956a653a75a
verdantpath <m_dittmann@yahoo.com>
2024-08-23 18:57:46 -0700

    Update login view

--- vue-3/muso-ninjas/src/views/auth/Login.vue
+++ vue-3/muso-ninjas/src/views/auth/Login.vue
@@ -10,16 +10,19 @@
 </template>
 <script>
 import useLogin from "@/composables/useLogin"
+// import { useRouter } from 'vue-router'
 import { ref } from "vue";
   export default {
     setup() {
       const { error, login, isPending } = useLogin()
+      // const router = useRouter()
 
       const email = ref('')
       const password = ref('')
 
       const handleSubmit = async () => {
         const res = await login(email.value, password.value)
+        // router.push({ name: 'home' })
         if(!error.value) {
           console.log('user logged in') 
         }
