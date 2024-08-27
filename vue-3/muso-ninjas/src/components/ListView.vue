a3743fb0f031bfbcc96697e4352128369662d103
verdantpath <m_dittmann@yahoo.com>
2024-08-26 19:27:04 -0700

    Update listview component

--- vue-3/muso-ninjas/src/components/ListView.vue
+++ vue-3/muso-ninjas/src/components/ListView.vue
@@ -1,17 +1,19 @@
 <template>
   <div v-for="playlist in playlists" :key="playlist.id">
-    <div class="single">
-      <div class="thumbnail">
-        <img :src="playlist.coverUrl" alt="cover">
+    <router-link :to="{ name: 'PlaylistDetails', params: { id: playlist.id }}">
+      <div class="single">
+        <div class="thumbnail">
+          <img :src="playlist.coverUrl" alt="cover">
+        </div>
+        <div class="info">
+          <h3>{{ playlist.title }}</h3>
+          <p>Created by {{ playlist.userName }}</p>
+        </div>
+        <div class="song-number">
+          <p>{{ playlist.songs.length }}</p>
+        </div>
       </div>
-      <div class="info">
-        <h3>{{ playlist.title }}</h3>
-        <p>Created by {{ playlist.userName }}</p>
-      </div>
-      <div class="song-number">
-        <p>{{ playlist.songs.length }}</p>
-      </div>
-    </div>
+    </router-link>
   </div>
 </template>
 <script>
