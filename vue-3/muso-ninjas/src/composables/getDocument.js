a1f6881bf6893517c30ff979022d133999e7c152
verdantpath <m_dittmann@yahoo.com>
2024-08-31 14:02:55 -0700

    Add getDocument function

---
+++ vue-3/muso-ninjas/src/composables/getDocument.js
@@ -0,0 +1,31 @@
+import { ref, watchEffect } from 'vue'
+import { projectFirestore } from '../firebase/config'
+
+const getDocument = (collection, id) => {
+  const document = ref(null)
+  const error = ref(null)
+
+  let documentRef = projectFirestore.collection(collection).doc(id)
+
+    const unsub = documentRef.onSnapshot((doc) => {
+      if (doc.data()) {
+         document.value = {...doc.data(), id: doc.id}
+         error.value = null
+      } else {
+        error.value = 'that document does not exist'
+      }
+      
+    }, (err) => {
+      console.log(err.message)
+      error.value = 'could not fetch document'
+    })
+
+    watchEffect((onInvalidate) => {
+      // unsub from prev collection when watcher is stopped (component unmounted)
+      onInvalidate(() => unsub())
+    })
+
+    return { document, error }
+}
+
+export default getDocument
