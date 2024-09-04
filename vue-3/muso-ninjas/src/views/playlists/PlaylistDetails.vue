4395bc0b0348153a3164d6eb47066e3c0437ecfc
verdantpath <m_dittmann@yahoo.com>
2024-09-03 20:08:23 -0700

    Update playlist details view

--- vue-3/muso-ninjas/src/views/playlists/PlaylistDetails.vue
+++ vue-3/muso-ninjas/src/views/playlists/PlaylistDetails.vue
@@ -25,6 +25,8 @@ import getDocument from '@/composables/getDocument'
 import getUser from '@/composables/getUser'
 import { computed } from 'vue'
 import useDocument from '@/composables/useDocument'
+import useStorage from '@/composables/useStorage'
+import { useRouter } from 'vue-router'
 
 export default {
   props: ['id'],
@@ -32,13 +34,17 @@ export default {
     const { error, document: playlist } = getDocument('playlists', props.id)
     const { user } = getUser() 
     const { deleteDoc } = useDocument('playlists', props.id)
+    const { deleteImage } = useStorage()
+    const router = useRouter()
 
     const ownership = computed(() => {
       return playlist.value && user.value && user.value.uid == playlist.value.userId
     })
 
     const handleDelete = async () => {
+      await deleteImage(playlist.value.filePath)
       await deleteDoc()
+      router.push({ name: 'home' })
     }
 
     return { error, playlist, ownership, handleDelete }
