7a0e752f2e64a1cdddfb28d94e5d843fa2e8d856
verdantpath <m_dittmann@yahoo.com>
2024-10-20 11:04:35 -0700

    Update useSignup function

--- vue-3/muso-ninjas/src/composables/useSignup.js
+++ vue-3/muso-ninjas/src/composables/useSignup.js
@@ -21,7 +21,7 @@ const signup = async (email, password, displayName) => {
     return res
     
   } catch(err) {
-    console.log(err.message)
+    // console.log(err.message)
     error.value = err.message
     isPending.value = false
   }
