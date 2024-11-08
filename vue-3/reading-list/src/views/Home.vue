184910c32b7268706302cc4ff88dd79290ca4f20
verdantpath <m_dittmann@yahoo.com>
2024-11-07 23:57:30 -0800

    Add Home view

---
+++ vue-3/reading-list/src/views/Home.vue
@@ -0,0 +1,69 @@
+<template>
+  <div class="home">
+    <ul>
+      <li v-for="book in books" :key="book.id">
+        <div class="details">
+          <h3>{{ book.title }}</h3>
+          <p>By {{ book.author }}</p>
+        </div>
+        <div class="icon">
+          <span class="material-icons">favorite</span>
+        </div>
+      </li>
+    </ul>
+    <CreateBookForm />
+  </div>
+</template>
+
+<script>
+import CreateBookForm from '@/components/CreateBookForm'
+import getCollection from '@/composables/getCollection'
+
+// firebase imports
+
+export default {
+  name: 'Home',
+  components: { CreateBookForm },
+  setup() {
+    const { documents: books } = getCollection('books')
+
+    
+
+    
+
+    return { books }
+  }
+}
+</script>
+
+<style>
+.home ul {
+  padding: 0;
+}
+.home li {
+  list-style-type: none;
+  background: #fff;
+  padding: 10px;
+  border-radius: 6px;
+  margin-bottom: 12px;
+  display: flex;
+}
+.home li .details {
+  margin-right: auto;
+}
+.home li h3 {
+  margin: 0;
+  margin-bottom: 4px;
+}
+.home li h3:hover {
+  cursor: pointer;
+  text-decoration: line-through;
+}
+.home li p {
+  margin: 0;
+}
+.icon {
+  color: #bbbbbb;
+  cursor: pointer;
+}
+</style>
