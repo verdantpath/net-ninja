55b78e3e9b37f6420c11ff330e0ff0aff282bbdf
verdantpath <m_dittmann@yahoo.com>
2024-11-11 20:43:57 -0800

    Add firebase config

---
+++ vue-3/reading-list/src/firebase/config.js
@@ -0,0 +1,19 @@
+import { initializeApp } from 'firebase/app'
+import { getFirestore } from 'firebase/firestore'
+
+const firebaseConfig = {
+  apiKey: "AIzaSyATwSb5xnrhTp_B0ycaaj_YM_2M12YN2V8",
+  authDomain: "fb9-reading-list-2494d.firebaseapp.com",
+  projectId: "fb9-reading-list-2494d",
+  storageBucket: "fb9-reading-list-2494d.firebasestorage.app",
+  messagingSenderId: "619881946878",
+  appId: "1:619881946878:web:13c9fc20ef6aea3ca78faf"
+};
+
+// init firebase
+initializeApp(firebaseConfig)
+
+// init services
+const db = getFirestore()
+
+export { db }
