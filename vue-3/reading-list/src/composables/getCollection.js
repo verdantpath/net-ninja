71f76028f00decfada4aeec28c50b39f72a769d2
verdantpath <m_dittmann@yahoo.com>
2024-12-16 11:58:44 -0800

    Update getCollection function

--- vue-3/reading-list/src/composables/getCollection.js
+++ vue-3/reading-list/src/composables/getCollection.js
@@ -2,25 +2,32 @@ import { ref, watchEffect } from 'vue'
 
 // firebase imports
 import { db } from '../firebase/config'
-import { collection, onSnapshot } from 'firebase/firestore'
+import { collection, onSnapshot, query, where } from 'firebase/firestore'
 
-const getCollection = (c) => {
+const getCollection = (c, q) => {
   const documents = ref(null)
 
   // collection reference
   let colRef = collection(db, c)
+
+  if (q) {
+    console.log(...q)
+    colRef = query(colRef, where(...q))
+  }
 
   const unsub = onSnapshot(colRef, snapshot => {
     let results = []
     snapshot.docs.forEach(doc => {
       results.push({ ...doc.data(), id: doc.id })
     })
+    
+    // update values
     documents.value = results
   })
 
   watchEffect((onInvalidate) => {
-    onInvalidate(() => unsub())
-  })
+    onInvalidate(() => unsub());
+  });
 
   return { documents }
 }
