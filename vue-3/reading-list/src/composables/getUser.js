4daa4fc45574ea73a4b1f3d8a2d032c11a2da047
verdantpath <m_dittmann@yahoo.com>
2024-12-17 21:32:33 -0800

    Update getUser function

--- vue-3/reading-list/src/composables/getUser.js
+++ vue-3/reading-list/src/composables/getUser.js
@@ -9,7 +9,7 @@ const user = ref(auth.currentUser)
 
 // auth changes
 onAuthStateChanged(auth, _user => {
-  // console.log('User state change. Current user is:', _user)
+  console.log('User state change. Current user is:', _user)
   user.value = _user
 })
 
